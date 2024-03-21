ef7abf6ffaa4e382f42eb02d5183889783396276
zivilekut <zivilekut@gmail.com>
2024-03-21 14:24:12 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+/*Sukurti klasę Book.
+Properties: title, pages, releaseYear
+visi properties turi būti inkapsuliuoti.
+sukurti 3 objektus su reikšmėmis naudojantis tuščiu konstruktoriu, ir tris naudojantis pilnu konstuktoriu.
+sudėti juos į knygų masyvą.
+prasukti ciklą per masyvą ir atspausdinti knygas (naudojam getterius)*/
+
+
+    }
+}
